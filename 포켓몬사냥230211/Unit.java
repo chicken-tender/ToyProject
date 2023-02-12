be9235a947f4743a970681412cc49a771554bbaa
ygm815 <ygm815@naver.com>
2023-02-12 09:47:32 +0900

    포켓몬 필드

---
+++ 포켓몬사냥230211/Unit.java
@@ -0,0 +1,10 @@
+package 포켓몬사냥230211;
+
+abstract public class Unit {
+    protected String name;
+    protected int hp;
+    protected int power;
+    protected double pHit;
+    protected int specialPower;
+    protected boolean isDead;
+}
